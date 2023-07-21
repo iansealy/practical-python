8a9b90fb967316f35013c7e9a30d9e3bca525afc
Ian Sealy <git@iansealy.com>
2023-07-21 18:50:04 +0100

    Exercise 1.10: Making a table

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -23,5 +23,7 @@ while principal > 0:
     principal = principal * (1+rate/12) - payment
     total_paid = total_paid + payment
 
-print('Total paid', total_paid)
-print('Months paid', month)
+    print(month, round(total_paid, 2), round(principal, 2))
+
+print('Total paid', round(total_paid, 2))
+print('Months', month)
