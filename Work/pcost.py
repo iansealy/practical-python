4bcdf3d66dc7cc8278f65a5138f9433142705b5a
Ian Sealy <git@iansealy.com>
2023-07-25 22:35:21 +0100

    Exercise 1.32: Using a library function

--- Work/pcost.py
+++ Work/pcost.py
@@ -2,12 +2,14 @@
 #
 # Exercise 1.27
 
+import csv
+
 def portfolio_cost(filename):
     with open(filename, 'rt') as f:
+        rows = csv.reader(f)
         total = 0.0
-        headers = next(f).strip().split(',')
-        for line in f:
-            (name, shares, price) = line.strip().split(',')
+        headers = next(rows)
+        for name, shares, price in rows:
             try:
                 total += int(shares) * float(price)
             except ValueError:
