080a68ba4df406585a10c20974926bd52859bee2
Ian Sealy <git@iansealy.com>
2023-07-25 22:37:18 +0100

    Exercise 1.33: Reading from the command line

--- Work/pcost.py
+++ Work/pcost.py
@@ -3,6 +3,7 @@
 # Exercise 1.27
 
 import csv
+import sys
 
 def portfolio_cost(filename):
     with open(filename, 'rt') as f:
@@ -16,5 +17,10 @@ def portfolio_cost(filename):
                 print(f"Couldn't parse shares ({shares}) and/or price ({price})")
     return total
 
-cost = portfolio_cost('Data/missing.csv')
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = 'Data/portfolio.csv'
+
+cost = portfolio_cost(filename)
 print('Total cost:', cost)
