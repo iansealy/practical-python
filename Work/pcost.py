fbc03f6fe56bd626bb93d57c96e7aae2ba9c1510
Ian Sealy <git@iansealy.com>
2023-07-25 18:23:49 +0100

    Exercise 1.30: Turning a script into a function

--- Work/pcost.py
+++ Work/pcost.py
@@ -2,10 +2,14 @@
 #
 # Exercise 1.27
 
-with open('Data/portfolio.csv', 'rt') as f:
-    total = 0.0
-    headers = next(f).strip().split(',')
-    for line in f:
-        (name, shares, price) = line.strip().split(',')
-        total += int(shares) * float(price)
-print(f'Total cost {total:0.2f}')
+def portfolio_cost(filename):
+    with open(filename, 'rt') as f:
+        total = 0.0
+        headers = next(f).strip().split(',')
+        for line in f:
+            (name, shares, price) = line.strip().split(',')
+            total += int(shares) * float(price)
+    return total
+
+cost = portfolio_cost('Data/portfolio.csv')
+print('Total cost:', cost)
