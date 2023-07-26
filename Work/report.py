187532c4e9dcefdb869e9921ebab25f53ff6c50c
Ian Sealy <git@iansealy.com>
2023-07-26 19:22:02 +0100

    Exercise 2.6: Dictionaries as a container

--- Work/report.py
+++ Work/report.py
@@ -14,3 +14,15 @@ def read_portfolio(filename):
             holding = { "name" : row[0], "shares" : int(row[1]), "price" : float(row[2]) }
             portfolio.append(holding)
     return portfolio
+
+def read_prices(filename):
+    prices = {}
+
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        for row in rows:
+            try:
+                prices[row[0]] = float(row[1])
+            except IndexError:
+                pass
+    return prices
