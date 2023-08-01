16e91f91bb49b60944e1fcc42912453548426786
Ian Sealy <git@iansealy.com>
2023-08-02 00:42:29 +0100

    Exercise 3.14: Using more library imports

--- Work/pcost.py
+++ Work/pcost.py
@@ -5,25 +5,21 @@
 import csv
 import sys
 
+import report
+
+
 def portfolio_cost(filename):
-    with open(filename, 'rt') as f:
-        rows = csv.reader(f)
-        total_cost = 0.0
-        headers = next(rows)
-        for rowno, row in enumerate(rows, start=1):
-            record = dict(zip(headers, row))
-            try:
-                nshares = int(record['shares'])
-                price = float(record['price'])
-                total_cost += nshares * price
-            except ValueError:
-                print(f'Row {rowno}: Bad row: {row}')
+    portfolio = report.read_portfolio(filename)
+    total_cost = 0.0
+    for record in portfolio:
+        total_cost += record["shares"] * record["price"]
     return total_cost
+
 
 if len(sys.argv) == 2:
     filename = sys.argv[1]
 else:
-    filename = 'Data/portfolio.csv'
+    filename = "Data/portfolio.csv"
 
 cost = portfolio_cost(filename)
-print('Total cost:', cost)
+print("Total cost:", cost)
