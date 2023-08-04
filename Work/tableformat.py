0d93ab919d3ce863c78defc25c9916b1d471d6de
Ian Sealy <git@iansealy.com>
2023-08-04 02:07:36 +0100

    Exercise 4.6: Using Inheritance to Produce Different Output

--- Work/tableformat.py
+++ Work/tableformat.py
@@ -13,3 +13,48 @@ class TableFormatter:
         Emit a single row of table data.
         """
         raise NotImplementedError()
+
+
+class TextTableFormatter(TableFormatter):
+    """
+    Emit a table in plain-text format
+    """
+
+    def headings(self, headers):
+        for h in headers:
+            print(f"{h:>10s}", end=" ")
+        print()
+        print(("-" * 10 + " ") * len(headers))
+
+    def row(self, rowdata):
+        for d in rowdata:
+            print(f"{d:>10s}", end=" ")
+        print()
+
+
+class CSVTableFormatter(TableFormatter):
+    """
+    Output portfolio data in CSV format.
+    """
+
+    def headings(self, headers):
+        print(",".join(headers))
+
+    def row(self, rowdata):
+        print(",".join(rowdata))
+
+
+class HTMLTableFormatter(TableFormatter):
+    """
+    Output portfolio data in HTML format.
+    """
+
+    def headings(self, headers):
+        print("<tr><th>", end="")
+        print("</th><th>".join(headers), end="")
+        print("</th></tr>")
+
+    def row(self, rowdata):
+        print("<tr><td>", end="")
+        print("</td><td>".join(rowdata), end="")
+        print("</td></tr>")
