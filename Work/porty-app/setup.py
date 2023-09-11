98c10b3ca66e3b2011142f1a335346a9b4cb220c
Ian Sealy <git@iansealy.com>
2023-09-12 00:38:12 +0100

    Exercise 9.5: Make a package

---
+++ Work/porty-app/setup.py
@@ -0,0 +1,11 @@
+# setup.py
+import setuptools
+
+setuptools.setup(
+    name="porty",
+    version="0.0.1",
+    author="Ian Sealy",
+    author_email="git@iansealy.com",
+    description="Practical Python Code",
+    packages=setuptools.find_packages(),
+)
